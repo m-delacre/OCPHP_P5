70aa05dc025d18b2ad185420ea25cd63970cf12f
mdelacre <mdelacre@protonmail.com>
2023-07-06 23:23:02 +0200

    adding an example of the config file to connect to the database

---
+++ config/__config.php
@@ -0,0 +1,6 @@
+<?php
+//example of the config file you need to setup with your own values to connect to your database. 🦐
+define('DB_NAME', 'myDbName');
+define('DB_HOST', 'dbServer');
+define('DB_USER', 'login');
+define('DB_PASSWORD', 'password');
