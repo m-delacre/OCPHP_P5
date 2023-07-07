5b5d10a82c605f38eda9b92b456d76c39e3e35a6
mdelacre <mdelacre@protonmail.com>
2023-07-07 05:04:51 +0200

    created the render logic, started the homepage to test the render, autoload fix

--- config/autoload.php
+++ config/autoload.php
@@ -2,18 +2,17 @@
 spl_autoload_register(function($className){
     if(file_exists('controller/' . $className . '.php'))
     {
-        echo 'booooo';
-        require_once('controller/' . $className . '.php');
+        require_once('controller//' . $className . '.php');
         return;
     }
-    if(file_exists('model/' . $className . '.php'))
+    if(file_exists('model//' . $className . '.php'))
     {
-        require_once('model/' . $className . '.php');
+        require_once('model//' . $className . '.php');
         return;
     }
-    if(file_exists('view/' . $className . '.php'))
+    if(file_exists('view//' . $className . '.php'))
     {
-        require_once('view/' . $className . '.php');
+        require_once('view//' . $className . '.php');
         return;
     }
 });
