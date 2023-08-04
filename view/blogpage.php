de1a4f63b1811705aab72434110ea6d11908450b
mdelacre <mdelacre@protonmail.com>
2023-08-04 15:41:31 +0200

    fix display data

--- view/blogpage.php
+++ view/blogpage.php
@@ -4,7 +4,11 @@
         <?php foreach ($posts as $post) : ?>
             <article class="blogpage-card">
                 <h3 class="blogpage-card-title"><?= $post->getTitle(); ?></h3>
-                <p class="text-body-secondary blogpage-card-author">Auteur : <?= $post->getAuthorPseudo(); ?> - Date de publication : <?= $post->getDateFr(); ?></p>
+                <p class="text-body-secondary blogpage-card-author">Auteur : <?= $post->getAuthorPseudo(); ?>
+                    <?php if ($post->getLastUpdate() != null) : ?>
+                        - Dernière mise à jour : <?= $post->getLastUpdate() ?>
+                    <?php endif; ?>
+                </p>
                 <p class="blogpage-card-chapo"><?= $post->getChapo(); ?></p>
                 <a class="blogpage-card-btn card-link" href=<?= "./index.php?action=showPost&id=" . $post->getId(); ?>>Voir plus</a>
             </article>
